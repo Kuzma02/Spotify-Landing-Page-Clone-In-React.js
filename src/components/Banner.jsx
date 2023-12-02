695fdde060f9bd6db3346f65b381aaaafb40ea62
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2023-12-02 10:09:11 +0100

    “project_completed”

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='banner'>
+        <h1>Learning is <br />everything</h1>
+        <p>Millions of songs and podcasts. No card card needed.</p>
+        <button>GET SPOIFY FREE</button>
+    </section>
+  )
+}
+
+export default Banner
